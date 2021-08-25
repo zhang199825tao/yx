675321946a67448f71747b465c09e56137b25b5d
root <1932460752@qq.com>
2021-08-25 15:12:25 +0800

    这是我第一次提交应学项目 v1.0

--- src/test/java/com/baizhi/TestRabbitMQ.java
+++ src/test/java/com/baizhi/TestRabbitMQ.java
@@ -1,14 +1,15 @@
 package com.baizhi;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 public class TestRabbitMQ {
 
+    //生产者
     @Test
     public void testProvider() throws Exception {
 
@@ -25,6 +26,9 @@ public class TestRabbitMQ {
         factory.setUsername("zhang");
         //设置rabbitmq服务器密码
         factory.setPassword("zhang");
+        //设置ip解析过程中的超时时间
+        factory.setHandshakeTimeout(30000);
+
 
         //获取连接
         Connection connection = factory.newConnection();
@@ -51,6 +55,55 @@ public class TestRabbitMQ {
         //关闭连接
         channel.close();
         connection.close();
+    }
+
+
+    //消费者
+    @Test
+    public void testXiaofei() throws Exception {
+
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+
+        //设置rabbitmq服务器IP地址
+        factory.setHost("192.168.87.139");
+        //设置rabbitmq服务器连接端口
+        factory.setPort(5672);
+        //设置rabbitmq服务器虚拟主机
+        factory.setVirtualHost("2101");
+        //设置rabbitmq服务器用户名
+        factory.setUsername("zhang");
+        //设置rabbitmq服务器密码
+        factory.setPassword("zhang");
+        //设置ip解析过程中的超时时间
+        factory.setHandshakeTimeout(30000);
+
+
+        //获取连接
+        Connection connection = factory.newConnection();
+        //获取通道
+        Channel channel = connection.createChannel();
+        /**设置队列参数
+         * @param queue 队列名称  如果这个队列不存在，将会被创建
+         * @param durable 持久性：用来定义队列是否要持久化  true:持久化  false:不持久化
+         * @param exclusive 是否只能由创建者使用，其他连接不能使用。 true:独占队列  false:不独占队列
+         * @param autoDelete 是否自动删除（没有连接自动删除） true:自动删除   false:不自动删除
+         * @param arguments 队列的其他属性(构造参数)
+         */
+        channel.queueDeclare("javaQueues", false, false, false, null);
+
+        /**消费者消费消息
+         * @param queue 队列名称
+         * @param autoAck 是否自动应答。false表示consumer在成功消费过后必须要手动回复一下服务器，如果不回复，服务器就将认为此条消息消费失败，继续分发给其他consumer。
+         * @param callback 回调方法类，一般为自己的Consumer类
+         */
+        channel.basicConsume("javaQueues",true,new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                java.lang.String message = new String(body);
+                System.out.println("消费者获取消息： " + message);
+            }
+        });
     }
 
 }
