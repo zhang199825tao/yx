4ae3cbedc9639660836f39a0a81bd2810c2fa120
root <1932460752@qq.com>
2021-08-30 16:39:49 +0800

    这是我第一次提交应学项目 v1.0

--- src/main/java/com/baizhi/service/AdminServiceImpl.java
+++ src/main/java/com/baizhi/service/AdminServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
+
 @Service
 @Transactional
 public class AdminServiceImpl implements AdminService{
